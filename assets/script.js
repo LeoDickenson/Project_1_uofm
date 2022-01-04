4f6a7b62d28b4e1899b9cc1fd6766e194560d8b9
Ceebcheng <jonathan2075@gmail.com>
2022-01-04 13:32:11 -0600

    first api

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,21 @@
-// nuthin
+// var requestUrl = 'https://www.bandsintown.com/';
+
+// //Browser XMLHttpRequest, built in the browser and require more code.
+// var xhr = new XMLHttpRequest();
+// xhr.onreadystatechange = function () {
+//   if (xhr.readyState === XMLHttpRequest.DONE) {
+//     console.log('XMLHttpRequest Response \n-------------');
+//     console.log(xhr.response);
+//   }
+// };
+// xhr.open('GET', requestUrl);
+// xhr.send();
+
+// fetch("http://www.bandsintown.com/event/13722599?app_id=foo&artist=Skrillex&came_from=67")
+// .then(function(response){
+//     return response.json();
+// })
+
+// .then(function(data){
+//     console.log(data);
+// })
