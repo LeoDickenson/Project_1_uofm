ea1923a7ee93d4f4bcd35d1588eb3ed6f11ef8c4
Ceebcheng <jonathan2075@gmail.com>
2022-01-04 14:28:03 -0600

    first api

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,17 @@
-// var requestUrl = 'https://www.bandsintown.com/';
+fetch("https://rest.bandsintown.com/artists/Justin%20Beiber?app_id=510")
+.then(function(response){
+    return response.json();
+})
 
-// //Browser XMLHttpRequest, built in the browser and require more code.
-// var xhr = new XMLHttpRequest();
-// xhr.onreadystatechange = function () {
-//   if (xhr.readyState === XMLHttpRequest.DONE) {
-//     console.log('XMLHttpRequest Response \n-------------');
-//     console.log(xhr.response);
-//   }
-// };
-// xhr.open('GET', requestUrl);
-// xhr.send();
+.then(function(data){
+    console.log(data);
+})
 
-// fetch("http://www.bandsintown.com/event/13722599?app_id=foo&artist=Skrillex&came_from=67")
-// .then(function(response){
-//     return response.json();
-// })
+fetch("https://rest.bandsintown.com/artists/Maroon%205/events?app_id=510&date=upcoming")
+.then(function(response){
+    return response.json();
+})
 
-// .then(function(data){
-//     console.log(data);
-// })
+.then(function(data){
+    console.log(data);
+})
