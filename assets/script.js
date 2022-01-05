253ca1f08f14deb5a8fe0e6844b0c5140cd8d3fb
Ceebcheng <jonathan2075@gmail.com>
2022-01-05 11:00:08 -0600

    test

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,12 @@
 
-fetch("https://rest.bandsintown.com/artists/Justin%20Beiber?app_id=510")
-.then(function(response){
-    return response.json();
-})
+// fetch("https://rest.bandsintown.com/artists/Justin%20Beiber?app_id=510")
+// .then(function(response){
+//     return response.json();
+// })
 
-.then(function(data){
-    console.log(data);
-})
+// .then(function(data){
+//     console.log(data);
+// })
 
 fetch("https://rest.bandsintown.com/artists/Maroon%205/events?app_id=510&date=upcoming")
 .then(function(response){
